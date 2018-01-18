1b4bd2a1ff33bf6557ca5bd71125aba52432e263
Tarang Khandpur <TKnologic104@gmail.com>
2018-01-17 18:24:02 -0600

    Compilation Friendly xD

---
+++ Aurora_v1/Main/Switches.h
@@ -0,0 +1,11 @@
+
+#ifndef Switches_h    // Put these two lines at the top of your file.
+#define Switches_h    // (Use a suitable name, usually based on the file name.)
+
+// Place your main header code here.
+enum Button {NONE = 0, Btn1 = 1, Btn2 = 2, Btn3 = 3, Btn4 = 4, Btn5 = 5, Btn6 = 6, Btn7 = 7, BackBtn = 8};
+extern enum Button BtnPressed;
+
+
+#endif // _HEADERFILE_H    // Put this line at the end of your file.
+
