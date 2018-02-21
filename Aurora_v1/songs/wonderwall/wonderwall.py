4be1622eeb1f33258bcaf8689b537b2db52d4bac
Tarang Khandpur <TKnologic104@gmail.com>
2018-02-21 14:15:56 -0600

    slight chagnes to songs

--- Aurora_v1/songs/wonderwall/wonderwall.py
+++ Aurora_v1/songs/wonderwall/wonderwall.py
@@ -5,7 +5,17 @@ chords = dict([
 ('D'   , 'ExxAxxD0wG2rB3be3y'),
 ('A'   , 'ExxAxxD1rG1gB3be3y'),
 ('C'   , 'ExxA3gD2rG0wB3be3y'),
-('D/F#', 'ExxA0wD0wG2rB3be3y')
+('D/F#', 'ExxA0wD0wG2rB3be3y'),
+('--'  , '------------------'),
+('8th' , '345'),
+('Em7' , 'E0wA2_D2_G0wB3be3y-345'),
+('Em71', 'E0wA2rDxxGxxBxxexx'),
+('Gp0' , 'ExxAxxDxxG0wB3be3y'),
+('Gp1' , 'EgwA2rDxxGxxBxxexx'),
+('Gp2' , 'ExxAxxD0wG0wB3be3y'),
+('Gp4' , 'E3rA2gDxxGxxB3be3y'),
+('Dp0' , 'ExxAxxD0wG2rBxxexx'),
+('Dp1' , 'ExxAxxDxxG2_B3_e3_')
 ])
 
 f = open("chords.txt", "r+")
