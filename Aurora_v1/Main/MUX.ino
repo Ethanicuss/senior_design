f869b7d7c4ad7df80e631bf6923bdabda1fe5e94
Tarang Khandpur <TKnologic104@gmail.com>
2018-02-26 11:26:53 -0600

    added Mux code

---
+++ Aurora_v1/Main/MUX.ino
@@ -0,0 +1,37 @@
+const byte sensor = A0;  // where the multiplexer in/out port is connected
+
+// the multiplexer address select lines (A/B/C)
+const byte addressA = 11; // low-order bit
+const byte addressB = 12;
+const byte addressC = 13; // high-order bit
+
+void MuxSetup()
+  {
+  //Serial.println ("Starting multiplexer test ...");
+  pinMode (addressA, OUTPUT); 
+  pinMode (addressB, OUTPUT); 
+  pinMode (addressC, OUTPUT); 
+  }  // end of setup
+
+int readSensor (const byte which)
+  {
+  // select correct MUX channel
+  digitalWrite (addressA, (which & 1) ? HIGH : LOW);  // low-order bit
+  digitalWrite (addressB, (which & 2) ? HIGH : LOW);
+  digitalWrite (addressC, (which & 4) ? HIGH : LOW);  // high-order bit
+  // now read the sensor
+  return analogRead (sensor);
+  }  // end of readSensor
+
+void MuxLoop ()
+  {
+  // show all 8 sensor readings
+  for (byte i = 0; i < 7; i++)
+    {
+    Serial.print ("Sensor ");
+    Serial.print (i);
+    Serial.print (" reads: ");
+    Serial.println (readSensor (i));
+    }
+  //delay (1000);
+  }  // end of loop
